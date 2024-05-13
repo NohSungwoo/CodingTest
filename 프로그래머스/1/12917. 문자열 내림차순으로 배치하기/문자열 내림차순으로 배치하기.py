ba72caedcdf3fdbd457eaa50d5a7c60861e26019
NohSungwoo <164475356+NohSungwoo@users.noreply.github.com>
2024-05-13 20:50:20 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -0,0 +1,17 @@
+def solution(s) :
+    list_s = []
+    upper = []
+    lower = []
+    for i in range(len(s)) :
+        list_s.append(s[i])
+    list_s = list(reversed(sorted(list_s)))
+    print(list_s)
+    for i in list_s :
+        print(i)
+        if i.isupper() :
+            upper.append(i)
+        else : 
+            lower.append(i)
+    lower = lower + upper
+    answer = ''.join(lower)
+    return answer
