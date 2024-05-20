5e26944ec8fa6ad724568d65f3dff185e3ac8873
NohSungwoo <164475356+NohSungwoo@users.noreply.github.com>
2024-05-20 23:43:29 +0900

    [level 1] Title: 음양 더하기, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,7 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)) :
+        if signs[i] == False :
+            absolutes[i] = -absolutes[i]
+        answer += absolutes[i]
+    return answer
