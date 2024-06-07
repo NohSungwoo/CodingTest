27bca08aba36a14855df83b30a4a5198278c3d42
NohSungwoo <164475356+NohSungwoo@users.noreply.github.com>
2024-06-08 01:19:26 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    even = ''
+    odd = ''
+    for i in num_list :
+        if i % 2 == 0 :
+            even += str(i)
+        else :
+            odd += str(i)
+    answer = int(even) + int(odd)
+    return answer
