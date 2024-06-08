6e8b3048d7db04fe93a1246b7390b27ee4385655
NohSungwoo <164475356+NohSungwoo@users.noreply.github.com>
2024-06-09 01:11:22 +0900

    [level 0] Title: ad 제거하기, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,10 @@
+def solution(strArr):
+    answer = []
+    count = 0
+    for i in strArr :
+        count += 1
+        if 'ad' in i :
+            pass
+        else :
+            answer.append(i)
+    return answer
