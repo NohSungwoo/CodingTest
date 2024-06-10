6e9a9525817309760979cdd7f024901ada3fbb83
NohSungwoo <164475356+NohSungwoo@users.noreply.github.com>
2024-06-11 02:10:49 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    answer = 0
+    
+    if len(num_list) >= 11 :
+        for i in num_list :
+            answer += i
+    else : 
+        answer = 1
+        for i in num_list :
+            answer = answer * i
+    
+    return answer
