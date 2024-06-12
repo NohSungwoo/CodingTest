441581f3b1754d4154e25c18642f9447dadba7fa
NohSungwoo <164475356+NohSungwoo@users.noreply.github.com>
2024-06-13 01:19:30 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -0,0 +1,10 @@
+def solution(my_string, letter):
+    answer = ''
+    
+    for i in my_string :
+        if i == letter : 
+            pass
+        else :
+            answer += i
+    
+    return answer
