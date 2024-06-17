cbafb63d478e26d14e09ce209b0432fa01a901c4
NohSungwoo <164475356+NohSungwoo@users.noreply.github.com>
2024-06-17 09:39:00 +0900

    [level 0] Title: 접미사 배열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = []
+    count = 0
+    
+    for i in range(len(my_string)) :
+        answer.append(my_string[i:])
+    answer.sort()
+    
+    return answer
