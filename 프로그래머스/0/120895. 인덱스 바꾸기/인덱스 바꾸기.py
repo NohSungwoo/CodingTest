b98472a0b98d9a5458f2b19cb924785e82c8c4df
NohSungwoo <164475356+NohSungwoo@users.noreply.github.com>
2024-06-27 01:59:54 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,15 +1,8 @@
 def solution(my_string, num1, num2):
-    list_string = []
-    change_string = ''
-    
-    for i in my_string:
-        list_string += i
+    list_string = list(my_string)
     
     list_string[num1], list_string[num2] = list_string[num2], list_string[num1]
     
-    for i in list_string:
-        change_string += i
-    
-    return change_string
+    return ''.join(list_string)
     
     
