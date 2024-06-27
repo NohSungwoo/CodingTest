f2ded701864d7d8c59a988e9f284ff49da69af30
NohSungwoo <164475356+NohSungwoo@users.noreply.github.com>
2024-06-28 03:01:51 +0900

    [level 1] Title: 소수 찾기, Time: 2956.91 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def solution(n):
+    answer = 0
+    for num in range(2, n + 1):
+        if is_prime(num):
+            answer += 1
+    return answer
