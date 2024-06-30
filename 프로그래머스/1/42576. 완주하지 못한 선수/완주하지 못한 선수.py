e19edd1a0a0ab9fa757108bd2613feeab9897fb2
NohSungwoo <164475356+NohSungwoo@users.noreply.github.com>
2024-07-01 02:49:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.29 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,24 @@
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
+    hash_map = {}
     
-    for a, b in zip(participant, completion):
-        if a != b:
-            return a      
-    return participant[-1]
+    # 참가자 이름을 해시맵에 추가
+    for p in participant:
+        if p in hash_map:
+            hash_map[p] += 1
+        else:
+            hash_map[p] = 1
+            
+    # 완주한 참가자 이름을 해시맵에서 감소
+    for c in completion:
+        if c in hash_map:
+            hash_map[c] -= 1
+            
+    # 값이 0이 아닌 참가자를 찾음
+    for key in hash_map:
+        if hash_map[key] != 0:
+            return key
+
+# 예시
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
+print(solution(participant, completion))  # "leo"가 출력됨
